a31f7b01824de1642fbec18e1d5acba99439dd99
Duroy Calvin <duroy.calvin@gmail.com>
2024-11-02 12:30:59 +0300

    Mise en place du composant Features

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
 import Header from './components/Header.vue'
+import Features from './components/body/Features.vue'
 </script>
 
 <template>
   <Header></Header>
+
+  <main>
+    <Features></Features>
+  </main>
 </template>
 
 <style scoped>
---
+++ src/components/body/Features.vue
@@ -0,0 +1,60 @@
+<template>
+  <section class="flex items-center flex-col gap-[50px] md:gap-[80px] py-[80px] md:py-0">
+    <div class="order-1 max-w-[285px] md:max-w-[552px] flex flex-col gap-[7px] text-center">
+      <h2 class="text-primary-text p-[10px]">Features</h2>
+      <h4 class="text-second-text p-[10px]">Most calendars are designed for teams. <span class="hidden md:inline">Slate is designed for freelancers</span></h4>
+    </div>
+
+    <div class="md:order-2 order-3 grid grid-cols-1 md:grid-cols-3 gap-[75px] py-[18px] md:py-0 md:gap-[52px] md:max-w-[869px] max-w-[255px]">
+      <div class="flex flex-col gap-[10px] items-center text-center">
+        <span class="text-primary p-[14px]">
+          <svg width="47" height="47" viewBox="0 0 47 47" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M14.6042 0.625C18.3117 0.625 21.8673 2.0978 24.4889 4.7194C27.1105 7.34101 28.5833 10.8967 28.5833 14.6042C28.5833 17.9846 27.3888 21.0854 25.3808 23.5H46.375V46.375H23.5V25.3808C21.0854 27.3888 17.9846 28.5833 14.6042 28.5833C10.8967 28.5833 7.34101 27.1105 4.7194 24.4889C2.0978 21.8673 0.625 18.3117 0.625 14.6042C0.625 10.8967 2.0978 7.34101 4.7194 4.7194C7.34101 2.0978 10.8967 0.625 14.6042 0.625Z" fill="currentColor"/>
+          </svg>
+        </span>
+        <h3 class="text-primary-text p-[10px]">OpenType features Variable fonts</h3>
+        <p class="text-second-text px-[10px]">Slate helps you see how many more days you need to work to reach your financial goal.</p>
+      </div>
+
+      <div class="flex flex-col gap-[10px] items-center text-center">
+        <span class="text-primary px-[16px] py-[19px]">
+          <svg width="45" height="42" viewBox="0 0 45 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M18.2486 41.65C22.402 40.0167 21.492 35.5134 19.392 32.6667C17.3153 29.75 14.4453 27.7434 11.552 25.8067C9.48756 24.5055 7.64432 22.8829 6.09197 21C5.43863 20.23 4.10863 18.8067 5.46197 18.5267C6.83863 18.2467 9.21863 19.6 10.432 20.1134C12.5553 21 14.6553 22.0267 16.6153 23.24L18.972 19.2734C15.332 16.87 10.6653 14.7467 6.3253 14.1167C3.85197 13.7434 1.23863 14.2567 0.398634 16.94C-0.348033 19.25 0.841967 21.5834 2.1953 23.4034C5.39197 27.6734 10.362 29.7267 14.072 33.4134C14.8653 34.1834 15.822 35.0934 16.2886 36.1667C16.7786 37.1934 16.662 37.2634 15.5653 37.2634C12.672 37.2634 9.0553 35 6.69864 33.5067L4.34197 37.4734C7.91197 39.6667 13.8853 43.0967 18.2486 41.65ZM44.1253 5.25004C44.6386 4.7367 44.6386 3.8967 44.1253 3.4067L41.092 0.373369C40.8476 0.137831 40.5214 0.00622559 40.182 0.00622559C39.8425 0.00622559 39.5164 0.137831 39.272 0.373369L36.892 2.75337L41.7453 7.6067L44.1253 5.25004ZM21.1653 18.48V23.3334H26.0186L40.3686 8.98337L35.5153 4.13004L21.1653 18.48Z" fill="currentColor"/>
+          </svg>
+        </span>
+        <h3 class="text-primary-text p-[10px]">Design with real data</h3>
+        <p class="text-second-text px-[10px]">Slate helps you see how many more days you need to work to reach your financial goal.</p>
+      </div>
+      
+      <div class="flex flex-col gap-[10px] items-center text-center">
+        <span class="text-primary px-[12px] py-[14px]">
+          <svg width="49" height="47" viewBox="0 0 49 47" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M47.6366 4.76792L44.2308 1.36209C43.2904 0.37084 41.6383 0.37084 40.647 1.36209L17.8737 24.1354L24.8633 31.125L47.6366 8.35167C48.6279 7.36042 48.6279 5.70834 47.6366 4.76792ZM12.7904 28.5833C10.7681 28.5833 8.82864 29.3867 7.39868 30.8167C5.96871 32.2466 5.16536 34.1861 5.16536 36.2083C5.16536 39.5379 2.21703 41.2917 0.0820312 41.2917C2.42036 44.3925 6.4362 46.375 10.2487 46.375C12.9451 46.375 15.531 45.3039 17.4376 43.3973C19.3442 41.4906 20.4154 38.9047 20.4154 36.2083C20.4154 34.1861 19.612 32.2466 18.1821 30.8167C16.7521 29.3867 14.8126 28.5833 12.7904 28.5833Z" fill="currentColor"/>
+          </svg>
+        </span>
+        <h3 class="text-primary-text p-[10px]">Fastest way to take action</h3>
+        <p class="text-second-text px-[10px]">Slate helps you see how many more days you need to work to reach your financial goal.</p>
+      </div>
+    </div>
+
+    <div class="md:order-3 order-2 py-[7px] md:py-[52px]">
+        <div class="h-[205px] md:h-[658px] w-[375px] md:w-[1177px] flex items-center justify-center bg-[url('@/assets/images/mobile_video_screen.png')] md:bg-[url('@/assets/images/video_screen.png')] bg-no-repeat bg-cover bg-center rounded-[26px] md:rounded-[45px] overflow-hidden shadow-[0_8px_63px_0_rgba(0,0,0,0.15)]">
+          <button class="flex items-center justify-center text-light-text bg-primary w-[61px] md:w-[188px] h-[61px] md:h-[188px] rounded-full">
+              <svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-[15px] md:w-[48px] h-[15px] md:h-[48px]">
+                <g clip-path="url(#clip0_176_14)">
+                <path d="M4.40234 0.593994L43.4434 24.0186L4.40234 47.4432V0.593994Z" fill="currentColor"/>
+                </g>
+                <defs>
+                <clipPath id="clip0_176_14">
+                <rect width="46.8492" height="46.8492" fill="currentColor" transform="translate(0.496094 0.593994)"/>
+                </clipPath>
+                </defs>
+              </svg>
+            </button>
+        </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+</style>
