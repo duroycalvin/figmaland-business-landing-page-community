f492c942227d9689380123a80ed2833b0d0e4999
Duroy Calvin <duroy.calvin@gmail.com>
2024-11-02 12:25:12 +0300

    Mise en place du composant Header(avec le menu)

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <script setup>
+import Header from './components/Header.vue'
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <Header></Header>
 </template>
 
 <style scoped>
